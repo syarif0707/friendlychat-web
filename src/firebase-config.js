eb4bb0e8ba28498143ccdc000617076df8723d8d
syarif0707 <syarifzafri@gmail.com>
2025-01-02 21:46:58 +0800

    adding Google Authentication

---
+++ src/firebase-config.js
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyBXIYHzHRbBngpR6nKcwRCtEW3eUIMtr',
+    authDomain: 'friendlychat-a4b1f.firebaseapp.com',
+    databaseURL: 'https://PROJECT_ID.firebaseio.com',
+    projectId: 'friendlychat-a4b1f',
+    storageBucket: 'friendlychat-a4b1f.firebasestorage.app',
+    messagingSenderId: '607383984793',
+    appId: '1:607383984793:web:3f3dfba54ebcc9ccf3f608',
+    measurementId: 'G-MEASUREMENT_ID',
+  },
+};
